337cecd92716046115d61a1cd7f076b086ceb43d
Hara <hara10kei@gmail.com>
2019-12-18 15:12:19 +0900

    python周り経過

---
+++ self_package/category.py
@@ -0,0 +1,47 @@
+﻿#!/usr/bin/env python
+# coding: utf-8
+
+
+import json
+from typing import List, Any
+
+
+def name_change(path, change_list) -> object:
+    """getパラメータをカテゴリー名に変換する:
+        @param
+         path :jsonファイルを参照するための絶対パス
+         change_list:getパラメータから抽出した選択したカテゴリーのリスト
+        @return
+         select_categories :list[] 選択したカテゴリー名の変換後リスト
+        @except
+        FileExistsError:スルー
+    """
+
+    select_categories: List[Any] = []
+    try:
+        with open(path, 'r', encoding='UTF-8')as f:
+            category_table = json.load(f)
+        for param in change_list[]:
+            for tag in category_table:
+                if tag["id"] == param:
+                    select_categories.append(tag.keys())
+        return select_categories
+    except FileExistsError:
+        return FileExistsError
+
+
+def creat_category_table(self, path):
+    """カテゴリーテーブルの作成
+    　@param
+       path:jsonフォルダの絶対パス
+
+      @return
+       category_table:list型のカテゴリーテーブル
+    """
+
+    category_table: List[Any] = []
+    with open(path, 'r', encoding='UTF-8')as f:
+        categories_dic = json.load(f)
+    for categories in self.categories_list:
+        category_table.append(categories.keys())
+    return category_table
---
+++ self_package/study.py
@@ -0,0 +1,34 @@
+﻿#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.models import load_model
+import numpy as np
+from keras.preprocessing.image import img_to_array, load_img
+import os
+import json
+import glob
+
+
+def learning(self, model_path, img_path, category_table):
+    pred_list = []
+    # 学習済みモデルの読込
+    model = load_model(model_path)
+    # 画像の読込
+    img = img_to_array(load_img(img_path, target_size=(224, 224)))
+    # 0-1に変換
+    img_nad = img_to_array(img) / 255
+    # 4次元配列に
+    img_nad = img_nad[None, ...]
+    # 判定結果をリストに
+    pred_data = model.predict(self.img_nad, batch_size=1, verbose=0)
+    for pred in pred_data:
+        for score in pred:
+            pred_list.append(score)
+    # 判定結果とカテゴリリストで辞書型を生成
+    dic = {key: val for key, val in zip(category_table, pred_list)}
+    return dic
+
+# In[ ]:
