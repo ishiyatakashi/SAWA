8c8cb1c1d5428f1c557513bc194fa886641c76f9
なまえ <>
2020-02-04 13:35:03 +0900

    一応完成（動作確認前）

--- maindata/urls.py
+++ maindata/urls.py
@@ -12,11 +12,7 @@ urlpatterns = [
     path('file_select/', views.file_select, name='file_select'),
     path('camera/category/', views.category, name='category'),
     path('camera/', views.camera, name='camera'),
-    path('file_select/sendfile/', views.send_file, name='imageCR'),
     path('result/', views.result, name='result'),
-    path('camera/category/result2/', views.result2, name='result2'),  # ベータ版画面
-    path('file_select/sendfile/result2/', views.result2, name='result2'),  # ベータ版画面,
-
 ]
 
 urlpatterns += staticfiles_urlpatterns()
