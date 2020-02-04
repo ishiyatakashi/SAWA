be5fa1af07ae1b59c9ce436674efbfce261ad779
なまえ <>
2020-02-04 11:17:37 +0900

    ishiya SQL settei

--- maindata/models.py
+++ maindata/models.py
@@ -3,15 +3,15 @@ from django.db import models
 
 class Category(models.Model):
     """カテゴリ"""
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,primary_key=True)
 
     def __str__(self):
         return self.name
 
 class Entertainer(models.Model):
     """芸能人"""
-    page = models.IntegerField('ID', blank=True, default=0, )
-    name = models.CharField('大分類', max_length=255)
+    id = models.IntegerField('ID', blank=True, default=0,primary_key=True )
+    name = models.CharField('名前', max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
