c7abe48c27fb7f261c02397f86b4e1f2c432c57b
nanashi <nanashi@gmail.com>
2020-02-10 11:38:43 +0900

    views

--- maindata/static/maindata/js/selectBOX.js
+++ maindata/static/maindata/js/selectBOX.js
@@ -1,42 +1,10 @@
-const categorymaster = new Array('actorM');
-const category = new Array('actor')
-
-//全選択
-  function AllChecked(){
-    const check = document.form.all.checked;
-    for(let i = 0; i < categorymaster.length; i++){
-        if(document.form.elements[categorymaster[i]].checked === check){
-            document.form.elements[categorymaster[i]].checked = check;
-            CategoryAllChecked(categorymaster[i],category[i]);
-        }
-    }
-  }
-
-  function CategoryAllChecked(master,name){
-      const check = document.form.elements[master].checked;
-      for (let i = 0; i < document.form.elements[name].length; i++) {
-          document.form.elements[name].checked = check;
+//俳優の全チェック＆解除
+function ActorAllChecked(){
+      const check = document.getElementById("a_ch0").checked; //マスタ
+      const category = document.getElementsByName("actor"); //カテゴリ
+      console.log(check);
+      //長さ分だけループしてマスタと同じ状態にする
+      for (let i = 0; i < category.length; i++) {
+          category[i].checked = check;
       }
-  }
-  // 一つでもチェックを外すと「全て選択」のチェック外れる
- function　DisChecked(master,name){
-     const checks = document.form.elements[name];
-     let checksCount = 0;
-     for (let i=0; i<checks.length; i++){
-      if(checks[i].checked === false){
-        document.form.elements[master].checked = false;
-        document.form.all.checked = false;
-      }else{
-        checksCount += 1;
-        if(checksCount === checks.length){
-          document.form.elements[master].checked = true;
-          document.form.all.checked = true;
-          for(let j = 0; j < categorymaster.length; j++){
-              if(document.form.elements[categorymaster[j]].checked === false){
-                  document.form.all.checked = false;
-              }
-          }
-        }
-      }
-    }
   }
